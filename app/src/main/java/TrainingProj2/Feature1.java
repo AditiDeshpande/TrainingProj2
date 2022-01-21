458edc0f43a7fe04bfa89e5593f3b47fb692f12f
Aditi Deshpande <aditi@gitpractice.com>
2022-01-21 15:29:11 +0530

    Committing the Gradle Java Project Skeleton

---
+++ app/src/main/java/TrainingProj2/Feature1.java
@@ -0,0 +1,12 @@
+
+package TrainingProj2;
+
+public class Feature1{
+    public String getGreeting() {
+        return "Hello World! From Feature1";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Feature1().getGreeting());
+    }
+}
